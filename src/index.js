1e885f9650ef95722499fa6ffcafdedbc13c6e38
nataliarodmo <133712261+nataliarodmo@users.noreply.github.com>
2025-06-07 21:30:58 -0400

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ function generateFact(event){
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "cf0o37c8aaf1022e4beeb7d4de3tca0a";
     let prompt = `Can you please generate a fun fact about ${instructionsInput.value}?`;
-    let context = " You are a fun fact generator expert and your mission is to generate fun facts based on the prompt of not more than 4 lines in HTML format try to not repeat the facts. Only display the fun fact";
+    let context = " You are a fun fact generator expert and your mission is to generate real fun facts based on the prompt of not more than 4 lines, try to not repeat the facts. Only display the fun fact";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let factElement = document.querySelector("#fun-fact");
+    factElement.classList.remove("hidden");
+    factElement.innerHTML = `<div class="generating">Generating Fun Fact about ${instructionsInput.value} ⏳ </div>` ;
     axios.get(apiUrl).then(displayFact);
 
 }
